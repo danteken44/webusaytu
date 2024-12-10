68271199f5793460941d37c18df0e5a5e3d37123
danteken44 <kenstoppable514@gmail.com>
2024-12-10 15:07:26 +0800

    Update homepagescript.js

--- homepagescript.js
+++ homepagescript.js
@@ -1,1 +1,40 @@
+function navScroll()
+{
+    let nav = document.getElementById('nav');
+    if(window.scrollY >= 100)
+    {
+        nav.classList.add('navscrolled');
+    }else{
+        nav.classList.remove('navscrolled')
+    }
+}
 
+
+window.addEventListener('scroll', navScroll);
+
+
+if (window.history.replaceState ) {
+    window.history.replaceState( null, null, window.location.href );
+  }
+
+  function bookbtn()
+{
+    let book = document.getElementById('bookform');
+        book.classList.add('bookformactive');
+}
+
+function bookformhide()
+{
+    let book = document.getElementById('bookform');
+        book.classList.remove('bookformactive');
+}
+
+function booknotlogin(){
+    alert('You have to Log In first before booking!');
+               setTimeout(function(){
+                  window.location.href = 'index.php';
+               }, 100);
+            
+}
+               
+            
